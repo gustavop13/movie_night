d3e43d805a5450c758b2444b4434f5853e7110ab
gustavop13 <gustavop@email.arizona.edu>
2019-07-23 18:31:37 -0700

    counter example 1

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Counter(props) {
+  return (
+    <div>
+      <h1>I am a counter!</h1>
+      <p>Count: {props.count}</p>
+      <button onClick={props.onIncrementClick}>increment</button>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncrementClick: () => {
+      const action = { type: 'INCREMENT'};
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
