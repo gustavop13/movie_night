acd1ad0214b00639302a41a4c5549c994661317f
gustavop13 <gustavop@email.arizona.edu>
2019-07-24 20:33:18 -0700

    added websocket boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import { subscribeToTimer } from './api';
 
 import store from './store/';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
 
   render() {
     return (
